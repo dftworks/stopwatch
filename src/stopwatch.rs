61b3387a9f959556809cccfa6f4948d2cfbdd19f
Junwen Li <junwen.li@dftworks.com>
2020-06-26 17:11:38 -0400

    revise the meaning of start, stop and reset

--- src/stopwatch.rs
+++ src/stopwatch.rs
@@ -16,14 +16,19 @@ impl Stopwatch {
                     elapsed: Duration::new(0, 0), }
     }
 
-    pub fn reset(&mut self) {
+    pub fn start(&mut self) {
         self.start = SystemTime::now();
+    }
+
+    pub fn reset(&mut self) {
+    	self.start = SystemTime::now();
+	self.stop = self.start;
+    	self.elapsed = Duration::new(0,0);
     }
 
     pub fn stop(&mut self) {
         self.stop = SystemTime::now();
-        self.elapsed =
-            self.elapsed + self.stop.duration_since(self.start).unwrap();
+        self.elapsed += self.stop.duration_since(self.start).unwrap();
     }
 
     pub fn elapsed_as_seconds(&self) -> f64 {
@@ -35,6 +40,16 @@ impl Stopwatch {
 fn test_stopwatch() {
     let mut timer = Stopwatch::new("Test");
     std::thread::sleep(std::time::Duration::new(2, 0));
+    timer.stop();
+    println!("Timer {} : {}", timer.tag, timer.elapsed_as_seconds());	
+    std::thread::sleep(std::time::Duration::new(3,0));
+    timer.start();
+    std::thread::sleep(std::time::Duration::new(1,0));
+    timer.stop();
+    println!("Timer {} : {}", timer.tag, timer.elapsed_as_seconds());
+    timer.reset();
+    timer.start();
+    std::thread::sleep(std::time::Duration::new(1,0));
     timer.stop();
     println!("Timer {} : {}", timer.tag, timer.elapsed_as_seconds());
 }
